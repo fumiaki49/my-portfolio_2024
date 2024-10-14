6e3427a15809990f40b5b45c2c322b6fd88bd488
fumiaki49 <me.fk39@gmail.com>
2024-10-14 11:22:31 +0900

    遅延処理の追加

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,12 +1,24 @@
 "use client";
 
 import React from "react";
+import { useState, useEffect } from "react";
 import { Typewriter } from "react-simple-typewriter";
 
 function PageTitle(props) {
+  const [startTyping, setStartTyping] = useState(false);
+  const delayStart = 500;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartTyping(true);
+    }, delayStart);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <h1 className="page-title">
-      <Typewriter words={[props.word]} cursor typeSpeed={40} />
+      {startTyping && <Typewriter words={[props.word]} cursor typeSpeed={40} />}
     </h1>
   );
 }
