b9da9aeafe28afcd7f153fd242ccba75cd07f396
fumiaki49 <me.fk39@gmail.com>
2025-01-03 21:09:45 +0900

    記述の修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,13 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="global-nav ">
-      <div className="links inner">
-        <Link href="/#top">Home</Link>
-        <Link href="/profile/#top">Profile</Link>
-        <Link href="/#works">Works</Link>
-        <Link href="mailto:me.fk39&#64;gmail.com">Contact</Link>
+      <div className="inner">
+        <div className="links">
+          <Link href="/#top">Home</Link>
+          <Link href="/profile/#top">Profile</Link>
+          <Link href="/#works">Works</Link>
+          <Link href="mailto:me.fk39&#64;gmail.com">Contact</Link>
+        </div>
       </div>
     </header>
   );
